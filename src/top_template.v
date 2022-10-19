270cc6dc9df176ba91c3a9463d0fdbf1a85e77e8
James Rosenthal <42195089+jdrosent@users.noreply.github.com>
2022-10-19 19:31:56 +0200

    Create top_template.v

---
+++ src/top_template.v
@@ -0,0 +1,16 @@
+// Tiny Tapeout 02
+// Verilog Template
+//Note: available cells can be found in /src/cells.v
+//you can also implicitly declare cells using verilog syntax:
+//e.g. reg example_dff;
+//e.g. assign io_out[2] = usersignal & !dff_q;
+
+module githubusername_top_template(	  //prepend your github username to the top module
+	input  [7:0] io_in,		              //do not change port names 
+	output [7:0] io_out);
+	
+
+ 	//*********** Your Design Below ************
+
+
+endmodule
