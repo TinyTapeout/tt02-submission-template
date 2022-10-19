f9a000f224229174bbb68f68ea136bec4750f161
James Rosenthal <42195089+jdrosent@users.noreply.github.com>
2022-10-19 18:51:23 +0200

    Update rising_edge_detect.v

--- src/rising_edge_detect.v
+++ src/rising_edge_detect.v
@@ -6,17 +6,14 @@ module jdrosent_rising_edge_detect(
 	output [7:0] io_out);
 
 	wire dff_o, not_dff_o;
-
-	dff_cell re_detect (
-		.clk(io_in[0]),
-		.d(io_in[1]),
-		.q(dff_o),
-		.notq(not_dff_o));
+	reg dff;
 	
-	and_cell and1 (
-		.a(io_in[1]),
-		.b(not_dff_o),
-		.out(io_out[2]));
+	always @ (posedge io_in[0])
+		begin
+			dff_o <= io_in[1];	
+		end
+	
+	assign io_out[2] = io_in[1] & ~dff_o;
 	
 	assign io_out[0] = io_in[0];
 	assign io_out[1] = io_in[1];
