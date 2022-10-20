8bd2fb1c1bb928ddf2df8088488e4b4eb8e690c9
James Rosenthal <42195089+jdrosent@users.noreply.github.com>
2022-10-20 14:09:09 +0200

    Update template.v
    added default_netname to template

--- src/template.v
+++ src/template.v
@@ -1,5 +1,6 @@
 // TinyTapeout02: Verilog Template
 // Remember to include your top module name in the info.yaml file
+`default_netname none
 
 module githubusername_top( //prepend your github username
 	input  [7:0] io_in,    //leave the port names unchanged
