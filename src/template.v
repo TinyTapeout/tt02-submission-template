aba27f7a179c97695e1afcbad8fb95633a969827
James Rosenthal <42195089+jdrosent@users.noreply.github.com>
2022-10-20 14:08:11 +0200

    Update template.v
    Remove reference to cells.v

--- src/template.v
+++ src/template.v
@@ -1,9 +1,5 @@
 // TinyTapeout02: Verilog Template
 // Remember to include your top module name in the info.yaml file
-// Note: available cells can be found in /src/cells.v
-// you can also implicitly declare cells using verilog syntax:
-// e.g. reg example_dff;
-// e.g. assign io_out[2] = usersignal & !dff_q;
 
 module githubusername_top( //prepend your github username
 	input  [7:0] io_in,    //leave the port names unchanged
