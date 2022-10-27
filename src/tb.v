aecdd1c5aef8d3a63dffc6c91102d81b44525f6e
Matt Venn <matt@mattvenn.net>
2022-10-27 17:49:44 +0200

    comment

--- src/tb.v
+++ src/tb.v
@@ -1,6 +1,11 @@
 `default_nettype none
 `timescale 1ns/1ps
 
+/*
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
+
 module tb (
     // testbench is controlled by test.py
     input clk,
