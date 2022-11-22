9dcbca6e4e4068e403eaeccefd2cb39d69c5ce3d
Matt Venn <matt@mattvenn.net>
2022-11-22 16:35:09 +0100

    increase area and core utilisation. stop doing basic placement

--- src/config.tcl
+++ src/config.tcl
@@ -29,9 +29,8 @@ set ::env(BOTTOM_MARGIN_MULT) 2
 
 # absolute die size
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 90 120"
-set ::env(FP_CORE_UTIL) 45
-set ::env(PL_BASIC_PLACEMENT) {1}
+set ::env(DIE_AREA) "0 0 150 170"
+set ::env(FP_CORE_UTIL) 55
 
 set ::env(FP_IO_HLENGTH) 2
 set ::env(FP_IO_VLENGTH) 2
